2d1ef524c45ea3962db6ef68a78ed253e73749b5
Jagandeep Singh <jagansingh93@gmail.com>
2021-03-18 21:01:17 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
 import flask
+import pickle
 
-print('flask')
+app = Flask(__name__)
+model = pickle.load(open('final_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('web.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = ???
+
+    prediction = model.predict(features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Estimated price is $ {}'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
